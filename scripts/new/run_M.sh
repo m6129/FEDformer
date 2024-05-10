2d115a24194bbdc712dbd61744cfcc4953d51b49
Anton <104712265+m6129@users.noreply.github.com>
2024-05-10 15:28:09 +0300

    add new lite scripts

---
+++ scripts/new/run_M.sh
@@ -0,0 +1,61 @@
+export CUDA_VISIBLE_DEVICES=1
+
+#cd ..
+
+for model in FEDformer
+do
+
+for preLen in 96 192 336 720
+do
+
+
+
+# ETTh1
+python -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path ETTh1.csv \
+  --task_id ETTh1 \
+  --model $model \
+  --data ETTh1 \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len $preLen \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --d_model 512 \
+  --itr 3
+
+
+for preLen in 24 36 48 60
+do
+# illness
+python -u run.py \
+ --is_training 1 \
+ --root_path ./dataset/illness/ \
+ --data_path national_illness.csv \
+ --task_id ili \
+ --model $model \
+ --data custom \
+ --features M \
+ --seq_len 36 \
+ --label_len 18 \
+ --pred_len $preLen \
+ --e_layers 2 \
+ --d_layers 1 \
+ --factor 3 \
+ --enc_in 7 \
+ --dec_in 7 \
+ --c_out 7 \
+ --des 'Exp' \
+ --itr 3
+done
+
+done
+
